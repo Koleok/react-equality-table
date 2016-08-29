5f96b500d553326d753e792a240bbf715d920fd1
Koleok <kchamb3@gmail.com>
2016-08-29 13:33:07 -0400

    Initial commit after gen

---
+++ src/table.js
@@ -0,0 +1,27 @@
+/* eqslint-disableq max-leqn */
+
+const [eq, nt, qi] = ['=', '≠', '≅'];
+
+export default [
+  [eq, nt, qi, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, qi, nt],
+  [nt, eq, nt, qi, nt, nt, nt, nt, qi, nt, qi, nt, nt, nt, nt, qi, nt, qi, qi, nt, nt],
+  [qi, nt, eq, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, qi, nt],
+  [nt, qi, nt, eq, nt, nt, nt, nt, qi, nt, qi, nt, nt, nt, nt, qi, nt, qi, qi, nt, nt],
+  [nt, nt, nt, nt, eq, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [qi, nt, qi, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, qi, nt],
+  [nt, qi, nt, qi, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt, nt, nt, nt, qi, nt, nt],
+  [nt, nt, nt, nt, qi, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, qi, nt, qi, nt, nt, nt, nt, nt, nt, eq, nt, nt, nt, nt, qi, nt, qi, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, eq, qi, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, qi, eq, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, eq, nt, nt, nt, nt, nt, nt],
+  [nt, qi, nt, qi, nt, nt, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, qi, nt, qi, nt, nt, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, qi, nt, qi, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [qi, nt, qi, nt, nt, nt, nt, qi, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+  [nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt, nt],
+];
