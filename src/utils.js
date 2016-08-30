b9e04831f9880234ec2d0e7ef74e0f67f55292f2
Koleok <kchamb3@gmail.com>
2016-08-30 16:26:39 -0400

    Point free a thing!

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 /* eslint-disable eqeqeq */
 import r from 'ramda';
 
+const flipMap = r.flip(r.map);
 export const strict = r.curry((x, y) => x === y);
 export const loose = r.curry((x, y) => x == y);
 
@@ -8,7 +9,7 @@ export const makeStates =
   r.converge(r.zipObj, [r.identity, r.map(r.toString)]);
 
 export const makeMatrix =
-  (fs, xs) => r.map(r.map(r.__, xs), fs);
+  r.useWith(flipMap, [r.identity, flipMap]);
 
 export const eqMatrix = r.curry(
   (f, xs) => r.converge(makeMatrix, [r.map(f), r.identity])(xs)
